33bfba97ec18ad2293b0251492346565a5853650
connectamansaini <connect.amansaini@gmail.com>
2022-11-25 15:34:22 +0530

    Updated readme

--- lib/src/profile/views/profile_view.dart
+++ lib/src/profile/views/profile_view.dart
@@ -12,113 +12,151 @@ class ProfileView extends StatelessWidget {
     return Scaffold(
       body: DefaultTabController(
         length: 3,
-        child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            // These are the slivers that show up in the "outer" scroll view.
-            return <Widget>[
-              SliverAppBar(
-                pinned: true,
-                backgroundColor: Colors.transparent,
-                elevation: 0.0,
-                title: const Text(
-                  'amansa1n1',
-                ),
-                actions: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.add_box,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.menu,
-                    ),
-                  ),
-                ],
-                forceElevated: innerBoxIsScrolled,
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const ProfilePicRow(),
-                      verticalSizedBox,
-                      const Padding(
-                        padding: EdgeInsets.only(left: 8.0),
-                        child: Text('Aman Saini'),
-                      ),
-                      verticalSizedBoxSmall,
-                      const Padding(
-                        padding: EdgeInsets.only(left: 8.0),
-                        child: Text('Bio'),
-                      ),
-                      verticalSizedBox,
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: EditProfileRow(),
-                      ),
-                      SizedBox(
-                        height: 110.0,
-                        child: ListView.builder(
-                          itemCount: 15,
-                          scrollDirection: Axis.horizontal,
-                          physics: const BouncingScrollPhysics(),
-                          itemBuilder: (context, index) =>
-                              const HighlightItem(),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SliverOverlapAbsorber(
-                handle:
-                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                sliver: const SliverAppBar(
-                  toolbarHeight: 0.0,
-                  expandedHeight: 0.0,
-                  primary: false,
+        child: SafeArea(
+          child: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              // These are the slivers that show up in the "outer" scroll view.
+              return <Widget>[
+                SliverAppBar(
                   backgroundColor: Colors.transparent,
-                  pinned: true,
-                  bottom: TabBar(
-                    tabs: <Widget>[
-                      Tab(
-                        icon: Icon(Icons.grid_on),
+                  elevation: 0.0,
+                  title: const Text(
+                    'amansa1n1',
+                  ),
+                  actions: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add_box,
                       ),
-                      Tab(
-                        icon: Icon(Icons.slideshow),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.menu,
                       ),
-                      Tab(
-                        icon: Icon(Icons.account_box_outlined),
-                      ),
-                    ],
+                    ),
+                  ],
+                  forceElevated: innerBoxIsScrolled,
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const ProfilePicRow(),
+                        verticalSizedBox,
+                        const Padding(
+                          padding: EdgeInsets.only(left: 8.0),
+                          child: Text('Aman Saini'),
+                        ),
+                        verticalSizedBoxSmall,
+                        const Padding(
+                          padding: EdgeInsets.only(left: 8.0),
+                          child: Text('Bio'),
+                        ),
+                        verticalSizedBox,
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: EditProfileRow(),
+                        ),
+                        SizedBox(
+                          height: 110.0,
+                          child: ListView.builder(
+                            itemCount: 15,
+                            scrollDirection: Axis.horizontal,
+                            physics: const BouncingScrollPhysics(),
+                            itemBuilder: (context, index) =>
+                                const HighlightItem(),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ];
-          },
-          body: const TabBarView(
-            children: <Widget>[
-              Center(
-                child: Text("It's cloudy here"),
-              ),
-              Center(
-                child: Text("It's rainy here"),
-              ),
-              Center(
-                child: Text("It's sunny here"),
-              ),
-            ],
+                SliverOverlapAbsorber(
+                  handle:
+                      NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                  sliver: SliverAppBar(
+                    toolbarHeight: 0.0,
+                    expandedHeight: 0.0,
+                    primary: false,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    pinned: true,
+                    floating: true,
+                    bottom: const TabBar(
+                      indicatorColor: Colors.white,
+                      tabs: <Widget>[
+                        Tab(
+                          icon: Icon(Icons.grid_on),
+                        ),
+                        Tab(
+                          icon: Icon(Icons.slideshow),
+                        ),
+                        Tab(
+                          icon: Icon(Icons.account_box_outlined),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ];
+            },
+            body: const TabBarView(
+              children: [
+                CustomGridView(),
+                CustomGridView(),
+                CustomGridView(),
+              ],
+            ),
           ),
         ),
         // ListView(
         //   padding: const EdgeInsets.symmetric(horizontal: 10.0),
         //   children: [
         //
+      ),
+    );
+  }
+}
+
+class CustomGridView extends StatelessWidget {
+  const CustomGridView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Builder(
+        builder: (BuildContext context) {
+          return CustomScrollView(
+            key: const PageStorageKey<String>('Posts'),
+            slivers: <Widget>[
+              SliverOverlapInjector(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              ),
+              SliverGrid(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => Container(
+                    color: Colors.white,
+                  ),
+                  childCount: 22,
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 2,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
